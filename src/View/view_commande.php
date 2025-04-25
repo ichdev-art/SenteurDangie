92be372e1216a1f16613ccde77ead80152ccd941
Ichem <ichem76610@hotmail.com>
2025-04-25 13:47:27 +0200

    petit reglage des cards commande

--- src/View/view_commande.php
+++ src/View/view_commande.php
@@ -6,22 +6,23 @@ include_once '../../templates/nav.php';
 ?>
 
 
-<div class="commande-card">
-  <?php foreach ($command as $commande) { ?>
 
+<?php foreach ($command as $commande) { ?>
+  <div class="commande-card">
     <div class="commande-header">
       <h3 class="command">Commande n°<?= $commande['com_id'] ?></h3>
       <p>Passée le <?= $commande['com_dateCommande'] ?></p>
+
     </div>
 
-    <div class="commande-produits">
 
+    <div class="commande-produits">
+      <h3 class="dateLivraison">Date de livraison prévue : <?= $commande['com_dateLivraison'] ?></h3>
       <?php foreach ($commande['produits'] as $produit) { ?>
 
         <div class="produit">
           <img src="/assets/img/<?= $produit['pro_img'] ?>" alt="<?= $produit['pro_nom'] ?>">
           <div class="infos">
-            <h3 class="dateLivraison">Date de livraison prévue : <?= $commande['com_dateLivraison'] ?></h3>
             <p class="nom"><?= $produit['pro_nom'] ?></p>
             <p>Quantité : <?= $produit['comlig_quantité'] ?></p>
             <p>Prix : <?= $fmt->formatCurrency($produit['pro_prix'], "EUR") ?></p>
@@ -35,10 +36,10 @@ include_once '../../templates/nav.php';
     <div class="commande-footer">
       <p>Total : <?= $fmt->formatCurrency($commande['total'], "EUR") ?></p>
     </div>
-    
-  <?php } ?>
+  </div>
+<?php } ?>
 
-</div>
+
 
 
 <?php
